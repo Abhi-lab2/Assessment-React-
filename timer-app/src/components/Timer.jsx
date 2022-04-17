4175106dcbc07130c179050b622ccea9429f2318
Abhijeet Chaudhari <abhijeetchaudhari615@gmail.com>
2022-04-17 18:14:41 +0530

    updates

--- timer-app/src/components/Timer.jsx
+++ timer-app/src/components/Timer.jsx
@@ -21,27 +21,41 @@ function Timer() {
 
   //   Return
   return (
-    <div className="timer">
-      <div className="digit">
-        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
-        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
-        <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
-      </div>
-      <br />
-      <br />
-      <div className="buttons">
-        <button onClick={() => setTimerOn(true)} className="button_slide ani s">
-          Start
-        </button>
-        <button onClick={() => setTimerOn(false)} className="button_slide ani">
-          Stop
-        </button>
-        <button onClick={() => setTimerOn(true)} className="button_slide ani">
-          Resume
-        </button>
-        <button onClick={() => setTime(0)} className="button_slide ani reset">
-          Reset
-        </button>
+    <div>
+      <img
+        src="https://raw.githubusercontent.com/jan25/vscode-timer/master/assets/timer.png"
+        alt=""
+        height="100px"
+        width="100px"
+      />
+      <div className="timer">
+        <div className="digit">
+          <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+          <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+          <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+        </div>
+        <br />
+        <br />
+        <div className="buttons">
+          <button
+            onClick={() => setTimerOn(true)}
+            className="button_slide ani s"
+          >
+            Start
+          </button>
+          <button
+            onClick={() => setTimerOn(false)}
+            className="button_slide ani"
+          >
+            Stop
+          </button>
+          <button onClick={() => setTimerOn(true)} className="button_slide ani">
+            Resume
+          </button>
+          <button onClick={() => setTime(0)} className="button_slide ani reset">
+            Reset
+          </button>
+        </div>
       </div>
     </div>
   );
